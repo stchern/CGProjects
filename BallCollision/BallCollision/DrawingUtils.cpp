8f904ad9066998ef7e908c41e417339455c230f8
stchern <nastyachernyshovabbk00@mail.ru>
2022-11-25 15:12:50 +0300

    Add partitioning

--- BallCollision/BallCollision/DrawingUtils.cpp
+++ BallCollision/BallCollision/DrawingUtils.cpp
@@ -11,40 +11,30 @@ void DrawingUtils::draw_ball(sf::RenderWindow& window, const Ball& ball)
 void DrawingUtils::move_ball(Ball& ball, float deltaTime)
 {
 
-    ball.speedX = (ball.p.x + ball.r < WINDOW_X - 1 && (ball.p.x - ball.r) )? ball.speedX : -ball.speedX;
-    ball.speedY = (ball.p.y + ball.r < WINDOW_Y - 1 && (ball.p.y - ball.r) )? ball.speedY : -ball.speedY;
-
-//    ball.dir.x = (ball.p.x + ball.r < WINDOW_X - 1 && (ball.p.x - ball.r) )? ball.dir.x : -ball.dir.x;
-//    ball.dir.y = (ball.p.y + ball.r < WINDOW_Y - 1 && (ball.p.y - ball.r) )? ball.dir.y : -ball.dir.y;
-
-//    float dx = ball.dir.x * ball.speedX * deltaTime;
-//    float dy = ball.dir.y * ball.speedY * deltaTime;
-
     float dx = ball.velocity().x * deltaTime;
     float dy = ball.velocity().y * deltaTime;
 
-//    float dx = ball.velocity().x;
-//    float dy = ball.velocity().y;
+    //correct code should be without multiptying or division by radius, but it looks strange
 
-//    dx = (ball.p.x + ball.r + dx < WINDOW_X)? dx : WINDOW_X - ball.p.x - ball.r - 1;
-//    dy = (ball.p.y + ball.r + dy < WINDOW_Y)? dy : WINDOW_Y - ball.p.y - ball.r - 1;
+    if ( ball.p.x + 2.0f *  ball.r + dx  >= WINDOW_X ) {
+        ball.speedX = -ball.speedX;
+        dx = -ball.p.x + WINDOW_X - 2.0f * ball.r - 1;
+    }
 
-//    dx = (ball.p.x + ball.r + dx < WINDOW_X)? dx : WINDOW_X - ball.p.x - ball.r - 1;
-//    dy = (ball.p.y + ball.r + dy < WINDOW_Y)? dy : WINDOW_Y - ball.p.y - ball.r - 1;
+    if ( ball.p.y + 2.0f * ball.r + dy >= WINDOW_Y ) {
+        ball.speedY = -ball.speedY;
+        dy = -ball.p.y + WINDOW_Y - 2.0f * ball.r - 1;
+    }
 
-//    dx = (ball.p.x - ball.r + dx >= 0)? dx : -(ball.p.x - ball.r);
-//    dy = (ball.p.y - ball.r + dy >= 0)? dy : -(ball.p.y - ball.r);
+    if ( ball.p.x - ball.r / 4.0f + dx < 0 ) {
+        ball.speedX = -ball.speedX;
+        dx = -(ball.p.x - ball.r / 4.0f);
+    }
 
-//    dx = (ball.p.x + ball.r + dx < WINDOW_X)? dx : WINDOW_X - ball.p.x - ball.r - 1;
-//    dy = (ball.p.y + ball.r + dy < WINDOW_Y)? dy : WINDOW_Y - ball.p.y - ball.r - 1;
-
-
-    dx = (ball.p.x + ball.r + dx < WINDOW_X)? dx : WINDOW_X - ball.p.x - ball.r - 1;
-    dy = (ball.p.y + ball.r + dy < WINDOW_Y)? dy : WINDOW_Y - ball.p.y - ball.r - 1;
-
-    dx = (ball.p.x - ball.r + dx >= 0)? dx : -(ball.p.x - ball.r);
-    dy = (ball.p.y - ball.r + dy >= 0)? dy : -(ball.p.y - ball.r);
-
+    if ( ball.p.y - ball.r / 4.0f + dy < 0 ) {
+        ball.speedY = -ball.speedY;
+        dy = -(ball.p.y - ball.r / 4.0f );
+    }
 
     ball.p.x += dx;
     ball.p.y += dy;
@@ -59,5 +49,16 @@ void DrawingUtils::draw_fps(sf::RenderWindow& window, float fps)
     window.setTitle(str);
 }
 
+std::vector<sf::Vector2f> DrawingUtils::partitioning(size_t splitFrequency, int offset)
+{
+    std::vector<sf::Vector2f> endPositions;
 
+    for (size_t y_part = 0; y_part < splitFrequency; ++y_part)
+        for (size_t x_part = 0; x_part < splitFrequency; ++x_part) {
+            sf::Vector2f firstSectionEnd(WINDOW_X * (x_part + 1) / splitFrequency - 1 - offset, WINDOW_Y * (y_part + 1) / splitFrequency - 1 - offset);
+            endPositions.push_back(firstSectionEnd);
+        }
+
+    return endPositions;
+}
 
--- BallCollision/BallCollision/DrawingUtils.h
+++ BallCollision/BallCollision/DrawingUtils.h
@@ -5,7 +5,7 @@
 
 constexpr int WINDOW_X = 1024;
 constexpr int WINDOW_Y = 768;
-constexpr int MAX_BALLS = 70;
+constexpr int MAX_BALLS = 100;
 constexpr int MIN_BALLS = 30;
 
 
@@ -15,6 +15,7 @@ namespace DrawingUtils
 void draw_ball(sf::RenderWindow& window, const Ball& ball);
 void move_ball(Ball& ball, float deltaTime);
 void draw_fps(sf::RenderWindow& window, float fps);
+std::vector<sf::Vector2f> partitioning(size_t splitFrequency, int offset = 0);
 
 
 }
