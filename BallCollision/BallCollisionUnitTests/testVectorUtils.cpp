4ddc0c91e052ce31370f821fce8da896ba71a812
stchern <nastyachernyshovabbk00@mail.ru>
2022-12-08 19:34:30 +0300

    Add tests for vector utils

--- BallCollision/BallCollisionUnitTests/testVectorUtils.cpp
+++ BallCollision/BallCollisionUnitTests/testVectorUtils.cpp
@@ -1,6 +1,31 @@
 #include "testVectorUtils.h"
 
-TestVectorUtils::TestVectorUtils()
+void TestVectorUtils::testLengthVector201()
+{
+
+}
+
+void TestVectorUtils::testLengthVector202()
+{
+
+}
+
+void TestVectorUtils::testLengthVector203()
+{
+
+}
+
+void TestVectorUtils::testDot01()
+{
+
+}
+
+void TestVectorUtils::testDot02()
+{
+
+}
+
+void TestVectorUtils::testDot03()
 {
 
 }
--- BallCollision/BallCollisionUnitTests/testVectorUtils.h
+++ BallCollision/BallCollisionUnitTests/testVectorUtils.h
@@ -1,11 +1,25 @@
-#ifndef TESTVECTORUTILS_H
-#define TESTVECTORUTILS_H
+#ifndef TEST_VECTOR_UTILS_H
+#define TEST_VECTOR_UTILS_H
 
+#include <QTest>
 
-class TestVectorUtils
+class TestVectorUtils: public QObject
 {
+    Q_OBJECT
+
 public:
-    TestVectorUtils();
+    explicit TestVectorUtils(QObject *parent = 0): QObject(parent){
+    };
+
+private slots:
+    void testLengthVector201();
+    void testLengthVector202();
+    void testLengthVector203();
+
+    void testDot01();
+    void testDot02();
+    void testDot03();
 };
 
-#endif // TESTVECTORUTILS_H
+#endif // TEST_VECTOR_UTILS_H
+
